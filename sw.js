555a28e55c7d9a3431d89102c2effdfc04322258
lpascalis <lpascalis@gmail.com>
2025-10-05 20:11:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('sforzu-v8-1').then(c=>c.addAll(['./','./index.html','./app.js','./manifest.json','./assets/icon-192.png','./assets/icon-256.png','./assets/icon-384.png','./assets/icon-512.png'])));
-});
-self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+self.addEventListener('fetch',()=>{})
